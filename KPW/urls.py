2b377d60cd127b3f82035ba3262d365d9acd7242
Kamil Mówiński <xnasterx@gmail.com>
2015-03-14 19:37:19 +0100

    Django initial commit

--- KPW/urls.py
+++ KPW/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from KPW.views import HomePage
+from KPW.views import HomePage, SearchView
 
 urlpatterns = patterns('',
     url(r'^$', HomePage.as_view(), name='home'),
+    url(r'^search$', SearchView.as_view(), name='search'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- KPW/views.py
+++ KPW/views.py
@@ -1,6 +1,25 @@
 __author__ = 'Kamil Mówiński'
 from django.views.generic import TemplateView
+from nltk.corpus import wordnet as wn
 
 
 class HomePage(TemplateView):
-    template_name = 'base.html'
+    template_name = 'kpw/index.html'
+
+
+class SearchView(TemplateView):
+    template_name = 'kpw/index.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super(SearchView, self).get_context_data(**kwargs)
+        q = self.request.GET.get('q', None)
+        if q:
+            ctx['query_list'] = self.get_another_query(q)
+        return ctx
+
+    def get_another_query(self, q):
+        synsets = wn.synsets(q, pos=wn.NOUN)
+        ret = list()
+        for synset in synsets:
+            ret.append((synset.lexname(), synset.definition()))
+        return ret
